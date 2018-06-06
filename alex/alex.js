d233bc844a0db7a92df87a78757707cdaf4ba3cf
Grigory <grigirij.zalizniak.salazar@gmail.com>
2018-06-06 22:21:22 +0300

    v1/7 + сервис, контроллер, директива, фильтры

--- alex/alex.js
+++ alex/alex.js
@@ -5,6 +5,7 @@ Alex_APP.controller('alexCtrl', ['$scope', '$timeout', '$http', '$q', 'ngDialog'
     function ($scope, $timeout, $http, $q, ngDialog, alexandroService, grishanyaFactory) {
         var vm = this;
 
+        vm.grishDay = "na";
         vm.functionFromController = function (arg1) {
             return alexandroService.someUsefulFunction(arg1);
         };
@@ -20,8 +21,6 @@ Alex_APP.service('alexandroService', ['$rootScope', '$timeout', '$q', function (
         }
         return Math.round(input * 100) / 100;
     };
-
-
 }]);
 
 Alex_APP.factory('grishanyaFactory', ['$rootScope', '$timeout', '$q', function ($rootScope, $timeout, $q) {
@@ -38,13 +37,15 @@ Alex_APP.factory('grishanyaFactory', ['$rootScope', '$timeout', '$q', function (
 Alex_APP.directive('sashaDirective', ['alexandroService', function (alexandroService) {
     return {
         link: function (scope, element, attr) {
-            console.info("sashaDirective " + alexandroService.someUsefulFunction(attr['myCoolDirectiveArg1']));
+            var extendeElem = $(element);
+            extendeElem.css('color', attr['divColorArg']);
+            extendeElem.fadeOut(parseInt(attr['divTimeArg']));
         }
     }
 }]);
 
 Alex_APP.filter('myalexFilter1', ['alexandroService', function (alexandroService) {
     return function (input) {
-        return alexandroService.someUsefulFunction(input);
+        return input + 'fuck-fuck';
     };
 }]);
