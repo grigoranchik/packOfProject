4571aa43da0fe4e5ab32abc02a93571cbdabfda0
Grigory <grigirij.zalizniak.salazar@gmail.com>
2018-06-06 21:34:18 +0300

    v1/7 + сервис, контроллер, директива, фильтры

--- alex/alex.js
+++ alex/alex.js
@@ -1,17 +1,32 @@
 var Alex_APP = angular.module("alexApp", ['ngDialog']);
 
-Alex_APP.controller('alexCtrl', ['$scope', '$timeout', '$http', '$q', 'ngDialog', 'alexandroService',
+Alex_APP.controller('alexCtrl', ['$scope', '$timeout', '$http', '$q', 'ngDialog', 'alexandroService', 'grishanyaFactory',
 
-    function ($scope, $timeout, $http, $q, ngDialog, alexandroService) {
+    function ($scope, $timeout, $http, $q, ngDialog, alexandroService, grishanyaFactory) {
         var vm = this;
 
+        console.info("alexCtrl created.")
     }
 ]);
 
 Alex_APP.service('alexandroService', ['$rootScope', '$timeout', '$q', function ($rootScope, $timeout, $q) {
     var srv = this;
 
+    console.info("alexandroService created.")
 }]);
+
+Alex_APP.factory('grishanyaFactory', ['$rootScope', '$timeout', '$q', function ($rootScope, $timeout, $q) {
+
+    var myFactoryFn = function (someArgument) {
+        var srv = this;
+
+        console.info("grishanyaFactory created: " + someArgument)
+    };
+
+    return myFactoryFn;
+}]);
+
+
 
 Alex_APP.directive('sashaDirective1', [function () {
     return {
@@ -21,11 +36,8 @@ Alex_APP.directive('sashaDirective1', [function () {
     }
 }]);
 
-
 Alex_APP.filter('myalexFilter1', [function () {
-
     return function (input) {
-        input = input || '';
 
         return input;
     };
