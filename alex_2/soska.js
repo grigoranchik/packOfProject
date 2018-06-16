1117ab819c2027b97c20ba4010b41977c8ee4bc7
Grigory <grigirij.zalizniak.salazar@gmail.com>
2018-06-16 16:26:13 +0300

    all working

--- alex_2/soska.js
+++ alex_2/soska.js
@@ -1,14 +1,15 @@
 angular.module("sosiApp").service('sosiService', ['$rootScope', '$timeout', '$q', function ($rootScope, $timeout, $q) {
     var srv = this;
 
-    srv.giveMePososat = function (pososal) {
+    srv.giveMePososat = function (pososalCallBack) {
 
         setTimeout(function () {
             if (typeof pososal == 'function') {
                 var nasosal = [{
                     nasosano: Math.random()
                 }];
-                pososal();
+                console.info("Na konce: " + nasosal.nasosano);
+                pososalCallBack(nasosal);
             } else {
                 alert("Pshol nah.")
             }
