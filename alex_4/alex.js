6dd50578fdf131725d1f10797fbb53d5e3666851
Grigory <grigirij.zalizniak.salazar@gmail.com>
2018-06-22 19:22:54 +0300

    lazy loading - 1

--- alex_4/alex.js
+++ alex_4/alex.js
@@ -5,5 +5,21 @@ Alex_APP.controller('alexCtrl', ['$scope', '$timeout', '$http', '$q', 'ngDialog'
     function ($scope, $timeout, $http, $q, ngDialog) {
         var vm = this;
 
+        vm.itemsArray = [];
+
+
+        vm.loadMoreItems = function () {
+            var startIdx = vm.itemsArray.length;
+            for (var i = 0; i < 100; i++) {
+                vm.itemsArray.push({
+                    itemTitle: startIdx + " - " + Math.random()
+                });
+                startIdx++;
+            }
+
+            console.info("total number of items:  " + vm.itemsArray.length);
+        };
+
+        vm.loadMoreItems();
     }
 ]);
