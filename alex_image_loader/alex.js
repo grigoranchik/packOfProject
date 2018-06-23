3a2ac59309968f9901c98c30ee841fb644a4f864
Grigory <grigirij.zalizniak.salazar@gmail.com>
2018-06-23 16:37:32 +0300

    file loader - 1

---
+++ alex_image_loader/alex.js
@@ -0,0 +1,10 @@
+var Alex_APP = angular.module("alexApp", ['ngDialog']);
+
+Alex_APP.controller('alexCtrl', ['$scope', '$timeout', '$http', '$q', 'ngDialog',
+
+    function ($scope, $timeout, $http, $q, ngDialog) {
+        var vm = this;
+
+
+    }
+]);
