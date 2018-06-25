3ed4c2549214fe4e76cff29c4372115ed33cc066
Grigory <grigirij.zalizniak.salazar@gmail.com>
2018-06-25 13:05:58 +0300

    file loader - 1

--- alex_localization/alex.js
+++ alex_localization/alex.js
@@ -1,27 +1,21 @@
 var Alex_APP = angular.module("alexApp", ['ngDialog', 'ngResource']);
 
-// injection - grishanyaFactory - function, someService - object
 Alex_APP.controller('alexCtrl', ['$scope', '$timeout', '$http', '$q', 'ngDialog', '$resource',
     function ($scope, $timeout, $http, $q, ngDialog, $resource) {
 
         var vm = this;
 
-        vm.getTranslation = function ($scope, language) {
-            var languageFilePath = 'resources_' + language + '.json';
+        vm.selectedLanguage = 'en';
+
+        vm.translate = function () {
+            var languageFilePath = 'resources_' + vm.selectedLanguage + '.json';
             console.log(languageFilePath);
 
             $resource(languageFilePath).get(function (data) {
-
-                $scope.translation = data;
-
+                vm.translation = data;
             });
         };
 
-        vm.translate = function () {
-            vm.getTranslation($scope, vm.selectedLanguage);
-        };
-
-        vm.selectedLanguage = 'en';
         vm.translate();
     }
 ]);
